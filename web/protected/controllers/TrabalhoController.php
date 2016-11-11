225750a31e8ca7ea49d547cb334eb621be56ffa2
Tiago Mazzarollo de Paula <tiago.mazzarollo@ad.ufrgs.br>
2016-11-11 19:01:42 -0200

    Exporte habilitado

--- web/protected/controllers/TrabalhoController.php
+++ web/protected/controllers/TrabalhoController.php
@@ -148,16 +148,16 @@ class TrabalhoController extends BaseController {
 	}
 
 	public function actionUpdateVer($id){
-		$finalizadas = Finalizado::model()->findAll([
-	      'condition'=>"trabalho_id=$id AND exportado=0 AND conteudo IS NOT NULL",
-	      'limit'=>8,
-	    ]);
-	    foreach ($finalizadas as $f) {
-	       $conteudo = json_decode($f->conteudo,true);
-	       if(isset($conteudo['saidaFormatada'])){
-	        $this->export($id,$f,$conteudo['saidaFormatada'],basename($conteudo['arquivo']));
-	      }
-	    }
+		// $finalizadas = Finalizado::model()->findAll([
+	 //      'condition'=>"trabalho_id=$id AND exportado=0 AND conteudo IS NOT NULL",
+	 //      'limit'=>8,
+	 //    ]);
+	 //    foreach ($finalizadas as $f) {
+	 //       $conteudo = json_decode($f->conteudo,true);
+	 //       if(isset($conteudo['saidaFormatada'])){
+	 //        $this->export($id,$f,$conteudo['saidaFormatada'],basename($conteudo['arquivo']));
+	 //      }
+	 //    }
 
 		$erros = Erro::model()->findAll("trabalho_id = $id");
 		if(count($erros) > 0){
@@ -220,36 +220,36 @@ class TrabalhoController extends BaseController {
 		}
 
 	  private function export($id,$controleExportada,$valor,$NomeArquivo){
-	  //     try {
-			// $trabalho = Trabalho::model()->findByPk((int) $id);
-			// if(is_null($trabalho)){
-			//   throw new Exception('', 1);
-			// } else {
-			// 	$export = json_decode($trabalho->export,true);
-			// 	$export = array_map(function($i) use($valor) {
-			// 		return $valor[$i];
-			// 	},$export);
+	      try {
+			$trabalho = Trabalho::model()->findByPk((int) $id);
+			if(is_null($trabalho)){
+			  throw new Exception('', 1);
+			} else {
+				$export = json_decode($trabalho->export,true);
+				$export = array_map(function($i) use($valor) {
+					return $valor[$i];
+				},$export);
 
-			// 	$model = new Leitura();
-			// 	$model->NomeArquivo = substr($NomeArquivo, 0,-4);
-			// 	$model->attributes = $export;
+				$model = new Leitura();
+				$model->NomeArquivo = substr($NomeArquivo, 0,-4);
+				$model->attributes = $export;
 
-			// 	if($model->validate()){
-			// 	  if($model->save()){
-			// 	    $controleExportada->exportado=1;
-			// 	    $controleExportada->update(['exportado']);
-			// 	  }
-			// 	} else {
-			// 	  throw new Exception(json_encode($model->getErrors()), 1);
-			// 	}
-			// }
-	  //     } catch(Exception $e){
-	  //       $erro = new Erro;
-	  //       $erro->trabalho_id = $id;
-	  //       $erro->texto = $e->getMessage() . ' | ' . json_encode($e);
-	  //       $erro->read = 0;
-	  //       $erro->save();
-	  //     }
+				if($model->validate()){
+				  if($model->save()){
+				    $controleExportada->exportado=1;
+				    $controleExportada->update(['exportado']);
+				  }
+				} else {
+				  throw new Exception(json_encode($model->getErrors()), 1);
+				}
+			}
+	      } catch(Exception $e){
+	        $erro = new Erro;
+	        $erro->trabalho_id = $id;
+	        $erro->texto = $e->getMessage() . ' | ' . json_encode($e);
+	        $erro->read = 0;
+	        $erro->save();
+	      }
 	  }
 
 	  public function actionVerErros($id)
