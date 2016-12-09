cfa4ef954e50214883f7d78c82ce5dc8a08d4466
Tiago Mazzarollo <tmazza@email.com>
2016-12-09 15:12:33 -0200

    Integração com interface: geração de template.

--- web/protected/config/database.php
+++ web/protected/config/database.php
@@ -1,9 +1,9 @@
 <?php
 return array(
     'class' => 'CDbConnection',
-    'connectionString' => 'mysql:host=localhost;port=3389;dbname=tarsius',
-    'username' => 'app',
-    'password' => 'acf729fb32637844df1dbd9c',
+    'connectionString' => 'mysql:host=localhost;dbname=tarsius',
+    'username' => 'root',
+    'password' => '345',
     'emulatePrepare'=>true,  // necessário em algumas instalações do MySQL
     // 'connectionString'    => "sqlite:".__DIR__.'/../../tarsius.db',
 );
